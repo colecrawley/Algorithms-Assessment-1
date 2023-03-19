471d6e222c48eb77de414fcb800eb7ebedf5bef9
colecrawley <126787278+colecrawley@users.noreply.github.com>
2023-03-19 22:27:15 +0000

    Add files via upload

---
+++ algorithms assessment 1/Algorithms_Assessment1/Algorithms_Assessment1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms_Assessment1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] roads = System.IO.File.ReadAllLines(@"/Users/colecrawley/Documents/algorithms assessment 1/roads/Road_1_256.txt");
+        Console.WriteLine("These are all road codes\n");
+
+        List<int> introads = roads.Select(int.Parse).ToList();
+
+        foreach (int introad in introads)
+        {
+            Console.WriteLine("\n" + introad);
+
+
+
+        }
+
+
+    }
+}
+
