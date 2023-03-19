dd42cfe509e8f78e179fb6b2c906952ef81cff08
colecrawley <26430829@students.lincoln.ac.uk>
2023-03-19 23:26:13 +0000

    implemented the bubble sort algorithm

--- algorithms assessment 1/Algorithms_Assessment1/Algorithms_Assessment1/Program.cs
+++ algorithms assessment 1/Algorithms_Assessment1/Algorithms_Assessment1/Program.cs
@@ -6,16 +6,28 @@ class Program
         string[] roads = System.IO.File.ReadAllLines(@"/Users/colecrawley/Documents/algorithms assessment 1/roads/Road_1_256.txt");
         Console.WriteLine("These are all road codes\n");
 
-        List<int> introads = roads.Select(int.Parse).ToList();
+        int[] roadnums = Array.ConvertAll(roads, int.Parse);
+        //List<int>introads = roads.Select(int.Parse).ToList();
 
-        foreach (int introad in introads)
+        foreach (int roadnum in roadnums)
         {
-            Console.WriteLine("\n" + introad);
-            Console.WriteLine("hello world");
-
-
-
+            Console.WriteLine("\n" + roadnum);
         }
+
+        var sortingmethod = new algorithms();
+        sortingmethod.Roadarray = roadnums;
+
+        var sortedroads = sortingmethod.Bubblesort();
+
+        Console.WriteLine("This is the bubble sort method\n");
+
+        int count = 0;
+        foreach (var road in sortedroads)
+        {
+            Console.WriteLine(road);
+            count++;
+        }
+        Console.WriteLine("The amount of steps it took to sort this list is: " + count);
 
 
     }
---
+++ algorithms assessment 1/Algorithms_Assessment1/Algorithms_Assessment1/algorithms.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Algorithms_Assessment1
+{
+	public class algorithms
+	{
+		public int[]? Roadarray { get; set; }
+
+		public int[] Bubblesort()
+		{
+			var n = Roadarray.Length;
+
+			for (int i = 0; i < n - 1; i++)
+				for (int j = 0; j < n - 1; j++)
+					if (Roadarray[j] > Roadarray[j + 1])
+					{
+						var temp = Roadarray[j];
+						Roadarray[j] = Roadarray[j + 1];
+						Roadarray[ j + 1] = temp;
+					}
+			return Roadarray;
+
+			
+		}
+	}
+}
+
